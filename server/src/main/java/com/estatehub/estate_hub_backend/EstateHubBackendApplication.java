a983dc3f2dfd4449404fedd877930faaea1f5c9c
PierreDevC <pscypre@gmail.com>
2025-07-19 16:50:42 -0400

    springboot backend init

---
+++ server/src/main/java/com/estatehub/estate_hub_backend/EstateHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estatehub.estate_hub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateHubBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/estatehub/estate_hub_backend/EstateHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estatehub.estate_hub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateHubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
